64e3dba45e7688acfa6a7b7595696ef28f31f7f3
Pedro Cobos <pedro.cobos@live.com>
2022-07-06 22:59:42 -0500

    add Delegate vote contract

---
+++ scripts/Ballot/delegateMyVote.ts
@@ -0,0 +1,54 @@
+import { Contract, ethers } from "ethers";
+import "dotenv/config";
+import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
+// eslint-disable-next-line node/no-missing-import
+import { Ballot } from "../../typechain";
+
+// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
+// Do never expose your keys like this
+const EXPOSED_KEY =
+  "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+
+  console.log(`Using address ${wallet.address}`);
+  const provider = ethers.providers.getDefaultProvider("ropsten");
+  const signer = wallet.connect(provider);
+  if (process.argv.length < 3) throw new Error("Ballot address missing");
+  const ballotAddress = process.argv[2];
+  if (process.argv.length < 4) throw new Error("Delegated address missing");
+  const delegatedAddress = process.argv[3];
+
+  console.log(
+    `Attaching ballot contract interface to address ${ballotAddress}`
+  );
+  const ballotContract: Ballot = new Contract(
+    ballotAddress,
+    ballotJson.abi,
+    signer
+  ) as Ballot;
+
+  const chairpersonAddress = await ballotContract.chairperson();
+  if (chairpersonAddress !== signer.address)
+    throw new Error("Caller is not the chairperson for this contract");
+
+  // make sure the address has the right to vote
+  const voter = await ballotContract.voters(delegatedAddress);
+  if (voter.weight.toNumber() < 1)
+    throw new Error("This address does not have the right to vote!");
+
+  const delegate = await ballotContract.delegate(delegatedAddress);
+  await delegate.wait();
+  console.log("Delegated!");
+  const voterDelegated = await ballotContract.voters(delegatedAddress);
+  console.log(
+    "The delegated address has a voting weight of",
+    voterDelegated.weight.toNumber()
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
