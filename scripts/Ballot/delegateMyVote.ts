7bbedc1fd09164ab9b5c07b25e3c62906ded281c
Pedro Cobos <pedro.cobos@live.com>
2022-07-06 23:04:34 -0500

    feat(back): add ternary to instanciate wallet from private key or mnemonic phrase

--- scripts/Ballot/delegateMyVote.ts
+++ scripts/Ballot/delegateMyVote.ts
@@ -10,7 +10,10 @@ const EXPOSED_KEY =
   "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
 
 async function main() {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
+  const wallet =
+    process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+      ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+      : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
 
   console.log(`Using address ${wallet.address}`);
   const provider = ethers.providers.getDefaultProvider("ropsten");
